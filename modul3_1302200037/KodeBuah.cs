5ef4869d1f0033586ede8dc131abdeedacb21049
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2022-03-12 16:05:38 +0700

    modul3_1302200037

---
+++ modul3_1302200037/KodeBuah.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302200037
+{
+    internal class KodeBuah
+    {
+        public static void getKodeBuah(string kel, string kode)
+        {
+            Console.WriteLine(kel + " " + kode);
+        }
+    }
+}
